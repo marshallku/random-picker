b33101d02076b2479d86b8a2737b5826b47a5f0c
Marshall Ku <marshall@kakao.com>
2024-06-30 17:25:56 +0900

    Add logics for running axum server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use axum::Router;
+use dotenv::dotenv;
+use log::info;
+use tokio;
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt()
+        .with_target(false)
+        .compact()
+        .init();
+
+    dotenv().ok();
+
+    let address = std::env::var("ADDRESS").unwrap_or_else(|_| "127.0.0.1".to_string());
+    let port = std::env::var("PORT").unwrap_or_else(|_| "48092".to_string());
+    let full_address = format!("{}:{}", address, port);
+    let listener = tokio::net::TcpListener::bind(full_address.as_str())
+        .await
+        .unwrap();
+    let app = Router::new();
+
+    info!("Server running at http://{}", full_address);
+
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap()
 }
