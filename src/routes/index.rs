275ca94b8fd3ad10f0be6700c590676667bb7012
Marshall Ku <marshall@kakao.com>
2024-07-01 22:43:59 +0900

    Add option to pick random item from array

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -1,5 +1,27 @@
-use axum::response::IntoResponse;
+use axum::{extract::Query, response::IntoResponse};
+use serde::Deserialize;
 
-pub async fn get_index() -> impl IntoResponse {
-    "Hello World!".into_response()
+use crate::utils::random::pick_random;
+
+#[derive(Deserialize)]
+pub struct IndexParams {
+    options: Option<String>,
+}
+
+pub async fn get_index(query: Query<IndexParams>) -> impl IntoResponse {
+    if query.options.is_none() {
+        return "No options provided".into_response();
+    }
+
+    let picked = pick_random(
+        &query
+            .options
+            .as_ref()
+            .unwrap()
+            .split(',')
+            .collect::<Vec<&str>>(),
+    )
+    .to_string();
+
+    picked.into_response()
 }
