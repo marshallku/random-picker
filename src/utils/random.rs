139236f2ff1bdaaca6191ce05d4870d78ebe0df3
Marshall Ku <marshall@kakao.com>
2024-07-02 23:47:29 +0900

    Improve randomness and safety

--- src/utils/random.rs
+++ src/utils/random.rs
@@ -1,4 +1,6 @@
+use rand::{thread_rng, Rng};
+
 pub fn pick_random<T>(array: &[T]) -> &T {
-    let index = rand::random::<usize>() % array.len();
+    let index = thread_rng().gen_range(0..array.len());
     &array[index]
 }
